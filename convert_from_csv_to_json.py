788a69de24a7c13b73eb41a6133fe1946731e52a
mongamae-nioh <jkawaoka@gmail.com>
2021-09-28 22:05:42 +0900

    update script convert from opendata to use alexa skill data

--- convert_from_csv_to_json.py
+++ convert_from_csv_to_json.py
@@ -5,8 +5,11 @@ import json
 import collections as cl
 import re
 
+# Number of garbage collection patterns
+number_of_collections = 46
+
 # ファイル
-inputfile = 'garvagecollectioncalendar202010.csv'
+inputfile = 'b9a4cf2b-8ffd-4be2-8431-f2d6ef129051'
 tempfile = 'temp.csv'
 outputfile = 'insert-dynamodb.json'
 
@@ -15,7 +18,7 @@ with open(inputfile, 'r', encoding='utf-8') as f:
     filedata = f.read()
 
 # DynamoDBへINSERTするために置換
-dic = {
+dict = {
     '日付': 'Date',
     '/': '-',
     '中央区': 'chuo-',
@@ -50,7 +53,11 @@ dic = {
     '-9,': '-09,'
 }
 
-dic2 = {
+# convert month and day format for dynamodb
+for key, value in dict.items():
+    filedata = filedata.replace(key, value)
+
+dict2 = {
     # 2020年8月のデータではヘッダが中央区①のような表示になったため置換対象を追加
     '①': '1',
     '②': '2',
@@ -61,25 +68,21 @@ dic2 = {
     '⑦': '7'
 }
 
-# convert month and day format for dynamodb
-for key, value in dic.items():
-    filedata = filedata.replace(key, value)
-
 # convert header format for dynamodb
-for key, value in dic2.items():
+for key, value in dict2.items():
     filedata = filedata.replace(key, value)
-
 
 # 中間ファイル生成
 with open(tempfile, 'w', encoding='utf-8') as f:
     f.write(filedata)
 
-# DynamoDBにINSERTしない"曜日"をカットするためのリスト作成
-list = list(range(2, 48))
-list.append(0)
+# スキルに不要な列以外を読み込む
+# オープンデータのカラム名はなぜか「曜」になっている
+exclude_columns = ["_id", "曜"]
+df = pd.read_csv(tempfile, encoding="utf-8", sep=',', usecols=lambda x: x not in exclude_columns)
 
-# "曜日"以外を読み込み
-df = pd.read_csv(tempfile, encoding="utf-8", sep=',',usecols=list)
+# format change(str -> date)
+df['Date'] = pd.to_datetime(df['Date'])
 
 # 札幌市のオープンデータのごみ番号をゴミスキル用に置換
 ## びん・缶・ペット 8 -> 4
@@ -93,17 +96,19 @@ df = df.fillna(0)
 
 # 中間ファイル
 df.to_csv(tempfile, index=False)
-
-#print(df)
+print(df)
 
 # 全エリアのDynamoDB INSERT用ファイル生成
 # 書き込まれる順番固定
 od = cl.OrderedDict()
 with open(outputfile, 'a') as f:
-    for i in range(1, 47):
+    for i in range(1, number_of_collections+1):
         df = pd.read_csv(tempfile, encoding="utf-8", sep=',',usecols=[0, i])
+#        print(df)
         columnsname = df.columns.values
         wardcalno = columnsname[1]
+#        print(wardcalno)
+
         for index, row in df.iterrows():
             # 小数点削除
             trashno = round(row[wardcalno])
@@ -115,13 +120,13 @@ with open(outputfile, 'a') as f:
             json.dump(od,f,indent=4, separators=(',', ': '))
 
 # DynamoDBへINSERTするためにjsonを整形
-dic2 = {
+dict3 = {
     '}': '},\n',
 }
 
 with open(outputfile, 'r', encoding='utf-8') as f:
     filedata = f.read()
-    for key, value in dic2.items():
+    for key, value in dict3.items():
         filedata = filedata.replace(key, value)
         # ファイルの先頭を [ へ置換
         result = re.sub('\A', '[\n', filedata)
